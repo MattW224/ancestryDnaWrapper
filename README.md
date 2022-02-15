0d186d8cbaba24908ab0254560a578a1e59d5a4c
Matt <m.wheeler.224@gmail.com>
2022-02-14 19:29:58 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# ancestryDnaWrapper
-Python wrapper to simplify calls to AncestryDNA API.
+# AncestryDNA API wrapper
+
+Ancestry exposes an undocumented REST API for its DNA features. This Python wrapper inventories the available calls, and exposes it to novice developers or power users in a more intuitive manner. I have consciously tried to keep the method names and parameters self-explanatory, but included the documentation below if additional context is needed.
+
+## Introduction
+Many DNA function calls require you to obtain and input GUIDs corresponding to tests and cousin matches. Later revisions of this wrapper might simplify this process, translating human-readable values to their GUIDs.
+
+This wrapper assumes intermediate knowledge of Python -- for example, being able to read the `ancestryDnaWrapper` class definition, and knowing to input the username and password values to authenticate successfully. 
+
+## Authentication
+Initializing the `ancestryDnaWrapper` class will automatically perform authentication. It will authenticate the US-based endpoint by default. This can be overrided with the `endpoint` keyword argument.
+
+## Selecting tests
+Any of the group, star, and test require a test to be selected. The wrapper will not default to anything.
+
+* `get_tests` -- will enumerate all completed tests registered to the account. By default, this will not include any tests shipped, or awaiting processing. To see other tests, change the `test_type` parameter (default value is `complete`).
+* `use_test` -- every object returned from `get_tests` will include a `guid` property. That property must be inputted to use any latter options. Like the Ancestry UI, you cannot select more than one test.
+
+## Test operations
+* `get_dna_matches` -- This will return **all** DNA matches. Every DNA match will contain a `testGuid` attribute. The method also contains a `shared_with_test_id` attribute. To obtain shared matches, input `testGuid` there.
+* `get_admixture`-- This will return your admixture (e.g. 100% Martian). The method also contains a `comparison_guid` attribute. To compare admixture with another user, input their `testGuid` there.
+
+## Group operations
+* `get_custom_groups` -- This will return all custom groups you created. If you do not have any, it will return an empty array. Each object will contain ` tagId` attribute -- required in any `delete` or `modify` operations. 
+* `create_custom_group` -- This creates a custom group for you to categorize your matches. Include a name and color value (e.g. `#FFFFFF`). The response object will include a `tagId` attribute, needed for addition/deletion operations.
+* `delete_custom_group` -- deletes group corresponding to the `tagId`. If creating, and then immediately deleting groups, you may encounter eventual consistency issues. Ensure you wait ~ 30 seconds before attempting operations.
+* `modify_group_membership` -- add or remove DNA matches to a custom group. Obtain the `testGuid` from `get_dna_matches` and the `tagId` from `create/get_custom_group`. Action will be `add` or `remove`.
+
+## Star operation
+* `modify_star` -- enables you to favorite a match by starring it. Obtain the `testGuid` from `get_dna_matches`. Action will be `add` or `remove`.
---
+++ core.py
@@ -0,0 +1,215 @@
+from collections import defaultdict
+import json
+import requests
+
+DEFAULT_ENDPOINT = "https://www.ancestry.com"
+
+# Any of these filters can be combined.
+INCLUSIVE_VALID_DNA_FILTERS = {
+    "commonancestors",
+    "messaged",
+    "newmatches",
+    "notviewed",
+    "notes",
+    "searchlocation",
+    "searchname",
+    "searchsurname",
+    "searchsimilarsurname"
+    "starredmatches"
+}
+
+# Exclusive in the sense that only one can be selected
+# from each group, unless minshareddna and maxshareddna.
+EXCLUSIVE_VALID_DNA_FILTERS = [
+    {"publictrees", "privatetrees", "unlinkedtrees"},
+    {"closematches", "distantmatches", "minshareddna", "maxshareddna"},
+    {"maternalid", "paternalid"}
+]
+VALID_DNA_SORTS = {"DATE", "RELATIONSHIP"}
+
+
+class ancestryDnaWrapper:
+    def __init__(self, username, password, endpoint=DEFAULT_ENDPOINT):
+        self._endpoint = endpoint
+        self._session = self._authenticate(username, password, endpoint)
+
+        self._matches_service = f"{endpoint}/discoveryui-matchesservice/api"
+
+    def _authenticate(self, username, password, endpoint):
+        credentials = json.dumps({"password": password, "username": username})
+        AUTH_HEADERS = {
+            "Content-Type": "application/json",
+            "User-Agent": ("Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+                            "AppleWebKit/537.36 (KHTML, like Gecko) "
+                            "Chrome/97.0.4692.99 Safari/537.36")
+        }
+
+        session = requests.session()
+        session.headers.update(AUTH_HEADERS)
+
+        response = session.post(
+            f'{endpoint}/account/signin/frame/authenticate',
+            data=credentials
+        )
+
+        if response.status_code == 200:
+            return session
+
+    def _web_request(self, method, url, payload=None, query_string=None):
+        web_request = getattr(self._session, method)
+        response = web_request(
+            url,
+            data=json.dumps(payload),
+            params=query_string)
+        return json.loads(response.text)
+
+    def _append_matches(self, collated_matches, match_groups):
+        for group in match_groups:
+            group_name = group["name"]["key"]
+            collated_matches[group_name].extend(group["matches"])
+
+        return collated_matches
+
+    def _round_next_multiple(self, multiple, value):
+        remainder = value % multiple
+
+        if remainder > 0:
+            return value + (multiple - remainder)
+        else:
+            return value + multiple
+
+    def get_tests(self, test_type="completeTests"):
+        response = self._session.get(f"{self._endpoint}/dna/secure/tests")
+        tests = json.loads(response.text)
+
+        filtered_tests = [test for test in tests["data"][test_type]]
+        return filtered_tests
+
+    def use_test(self, test_guid):
+        self._current_test = test_guid
+
+    def get_admixture(self, comparison_guid=None):
+        if comparison_guid:
+            endpoint = (f"{self._matches_service}/compare/{self._current_test}"
+                        f"/with/{comparison_guid}/ethnicity")
+        else:
+            endpoint = (f"{self._endpoint}/dna/secure/tests/"
+                        f"{self._current_test}/ethnicity")
+
+        return self._web_request('get', endpoint)
+
+    # Filter out all valid filters, then ensure set is empty.
+    def validate_filters(self, sort_type, filters):
+        if sort_type.upper() not in VALID_DNA_SORTS:
+            raise ValueError(
+                (f"sort_type value '{sort_type}' not accepted -- "
+                    "must be 'DATE' or 'RELATIONSHIP'."))
+
+        selected_filter_types = set(filters.keys())
+        selected_filter_types = selected_filter_types.difference(
+            INCLUSIVE_VALID_DNA_FILTERS)
+
+        for valid_filters in EXCLUSIVE_VALID_DNA_FILTERS:
+            intersection = selected_filter_types.intersection(valid_filters)
+
+            if len(intersection) == 2:
+                MIN_SHARED_DNA = "minshareddna"
+                MAX_SHARED_DNA = "maxshareddna"
+
+                assert MIN_SHARED_DNA in selected_filter_types
+                assert MAX_SHARED_DNA in selected_filter_types
+
+                selected_filter_types.remove(MIN_SHARED_DNA)
+                selected_filter_types.remove(MAX_SHARED_DNA)
+            elif len(intersection) == 1:
+                selected_filter_types.remove(intersection.pop())
+
+        if len(selected_filter_types) > 0:
+            raise ValueError(
+                (f"Filters {selected_filter_types} not accepted. "
+                 "See README for valid filter types."))
+
+    def get_dna_matches(
+            self,
+            sort_type="RELATIONSHIP",
+            filters={},
+            shared_with_test_id=None):
+        matches = defaultdict(list)
+
+        self.validate_filters(sort_type, filters)
+
+        filters.update({
+            "page": 1,
+            "sortby": sort_type
+        })
+
+        if shared_with_test_id:
+            filters.update({"relationguid": shared_with_test_id})
+
+        endpoint = (f"{self._matches_service}/samples/"
+                    f"{self._current_test}/matches/list")
+        result = self._web_request('get', endpoint, query_string=filters)
+
+        matches = self._append_matches(matches, result["matchGroups"])
+
+        while result["bookmarkData"]["moreMatchesAvailable"]:
+            previous_page = filters["page"]
+
+            # Round to next multiple of five. Each API call returns up to
+            # 200 results, with every fifty results constituting one page.
+            filters["page"] = self._round_next_multiple(5, previous_page)
+
+            filters.update(
+                {
+                    "bookmarkData": json.dumps({
+                        "moreMatchesAvailable": True,
+                        "lastMatchesServicePageIdx": previous_page
+                    })
+                }
+            )
+
+            result = self._web_request('get', endpoint, query_string=filters)
+            matches = self._append_matches(matches, result["matchGroups"])
+
+        return matches
+
+    def get_custom_groups(self):
+        endpoint = f"{self._matches_service}/samples/{self._current_test}/tags"
+        return self._web_request('get', endpoint)
+
+    def create_custom_group(self, group_name, group_hex_color):
+        endpoint = f"{self._matches_service}/samples/{self._current_test}/tags"
+        payload = {"tagName": group_name, "tagColor": group_hex_color}
+
+        return self._web_request('post', endpoint, payload)
+
+    def delete_custom_group(self, group_id):
+        # Don't ask why these are empty values -- beats me.
+        payload = {"removeTagFromAllMatchesResult": "", "deleteTagResult": ""}
+        endpoint = (f"{self._matches_service}/samples/"
+                    f"{self._current_test}/tags/{group_id}")
+
+        return self._web_request('delete', endpoint, payload)
+
+    def modify_group_membership(self, action, group_id, test_id):
+        action = action.lower().trim()
+
+        if action == "add":
+            endpoint = (f"{self._matches_service}/samples/"
+                        f"{self._current_test}/{group_id}")
+        elif action == "remove":
+            endpoint = (f"{self._matches_service}/samples/{self._current_test}"
+                        f"/matches/tags/{group_id}/remove")
+        else:
+            raise ValueError(
+                "action value not accepted -- must be 'add' or 'remove'.")
+
+        payload = {"matchSampleIds": [test_id]}
+        return self._web_request('post', endpoint, payload)
+
+    def modify_star(self, action, test_id):
+        endpoint = (f"{self._matches_service}/samples/"
+                    f"{self._current_test}/matches/{test_id}")
+        payload = {"starred": True} if action == "add" else {"starred": False}
+
+        return self._web_request('post', endpoint, payload)
